41157710710feb5dae8686ed617a0961c98e84f9
Matthew <162459734+Grispys@users.noreply.github.com>
2024-10-07 16:36:20 -0230

    fixed queries to concatenate the two names

--- TASK1queries.sql
+++ TASK1queries.sql
@@ -1,3 +1,3 @@
-SELECT student.first_name, student.last_name
+SELECT CONCAT(student.first_name, ' ', student.last_name) AS full_name
 FROM students student JOIN enrollments enrolled ON student.student_id = enrolled.student_id
 WHERE enrolled.course_id = 2;
